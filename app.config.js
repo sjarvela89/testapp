180c7c5cfbab1e23049fba451cfe88168a0c37de
sjarvela89 <sjarvela89@hotmail.com>
2025-05-21 20:52:06 +0300

    Add internet permissions.

--- app.config.js
+++ app.config.js
@@ -20,6 +20,7 @@ export default {
     },
     android: {
       adaptiveIcon: {
+        permissions: ["INTERNET"],
         foregroundImage: './assets/images/adaptive-icon.png',
         backgroundColor: '#ffffff',
       },
